5b4be8846324dd137608741a3b62970949b7e8f9
Kamila Duda <kakus86@o2.pl>
2020-07-21 17:16:44 +0200

    Updates

---
+++ precache-manifest.aaa8591f9460c10fc14ded7a028bbe34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d52127dd21fc095dc082768ebda121b",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "fa0fb7e1ebf3101d0cea",
+    "url": "/currency-converter-react/static/css/main.618dd0c0.chunk.css"
+  },
+  {
+    "revision": "df18eda2b5fc548341b9",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter-react/static/js/2.abfbd1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa0fb7e1ebf3101d0cea",
+    "url": "/currency-converter-react/static/js/main.f209705c.chunk.js"
+  },
+  {
+    "revision": "07b2973be230e9c120d7",
+    "url": "/currency-converter-react/static/js/runtime-main.2415008a.js"
+  }
+]);
