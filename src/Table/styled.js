ea7c153e298e67a11ca70b9fcceb57fe58bcc81f
Kamila Duda <kakus86@o2.pl>
2020-08-18 13:30:23 +0200

    Rounded corners in table

--- src/Table/styled.js
+++ src/Table/styled.js
@@ -1,32 +1,38 @@
 import styled from 'styled-components'
 
 export const TableContainer = styled.table`
-  border-collapse: collapse;
+  border-collapse: separate;
   width: 250px;
   font-size: 15px;
+  border: 1px solid #000;
+border-radius: 10px;
+border-spacing: 0px;
 `
-
 export const Caption = styled.caption`
   font-size: 13px;
 `
-
 export const TableRow = styled.tr`
-  border: 2px solid #000;
   padding: 5px;
 `
-
 export const TableHeading = styled.th`
-  border: 2px solid #000;
-  padding: 5px;
+  
   background-color: teal;
-  color: #fff;
+  padding: 5px;   
+&:first-child{
+  border-radius: 10px 0 0 0;
+}
+&:last-child{
+  border-radius: 0 10px 0 0;
+border-left: 1px solid #000; 
+}
 `
 
 export const TableCell = styled.td`
-  border: 2px solid #000;
-  padding: 5px;
-  background-color: #fff;
-  font-weight: 700;
+  padding: 5px;  
+border-top: 1px solid #000; 
+&:nth-child(even){
+border-left: 1px solid #000; 
+}
 `
 
 export const TableImage = styled.img`
